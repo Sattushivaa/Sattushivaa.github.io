e94fedb8effccc3b51c77ca631647e3bd6b66ea5
Satyam Bharti <73614498+Sattushivaa@users.noreply.github.com>
2024-03-25 16:06:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-console.log('Hello World!');
-let body = document.querySelector("h1") ; 
-let animator = new Animator (body ) ; 
-animator.animate(2000,500,"fast","efficient"); 
-let section_el = document.querySelectorAll ('section *');
-section_el.forEach( el => ScrollFade (el ,500));
+const el=(k)=>document.querySelector(k);
+document.onscroll = (ev)=>{
+  var scrolled = window.scrollY;
+  var vl = -(scrolled)+'px';
+  el('#moon').style.top = vl.replace('-','');
+  el('#rockright').style.right = 
+  el('#rockleft').style.left =
+ // el('#pebbleright').style.right = 
+  el('#pebbleleft').style.left = vl;
+}
 
-let sideScroll_el = document.querySelectorAll(".ss") ; 
-sideScroll_el.forEach(el=>ScrollSide(el,500));
+el('#moon').style.top = -window.scrollY+'px';
